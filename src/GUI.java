4b1de41a167b39c8c18a73987ccb157b271429e4
paliankit <paliankit999@gmail.com>
2025-03-31 22:27:09 +0530

    first commit, created window with scrollPane

---
+++ src/GUI.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+
+    JFrame window;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+
+
+    public static void main(String[] args){
+        new GUI();
+    }
+
+    public GUI(){
+        createWindow();
+        createTextArea();
+        window.setVisible(true);
+    }
+
+    public void createWindow(){
+        window=new JFrame("Notepad");
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void createTextArea(){
+        textArea=new JTextArea();
+        //window.add(textArea);
+        scrollPane=new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        window.add(scrollPane);
+
+    }
+}
